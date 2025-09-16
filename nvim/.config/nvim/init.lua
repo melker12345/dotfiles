c55c7d8ff87593a55dc3e06ffedbdbdac0fe22b8
melker12345 <memleoberg@gmail.com>
2025-09-16 11:25:01 +0200

    xremap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,8 +10,8 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.expandtab = true
-vim.o.shiftwidth = 8
-vim.o.tabstop = 8
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
 vim.o.termguicolors = true
 vim.o.scrolloff=8
 
